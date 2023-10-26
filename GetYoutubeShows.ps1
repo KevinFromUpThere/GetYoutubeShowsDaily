ee1d6af3c235d5f201e588bc80013d2b9ce58c56
Keivn Archibald <karchibald@gmail.com>
2023-10-26 14:53:56 -0400

    Adding Script To Repo

---
+++ GetYoutubeShows.ps1
@@ -0,0 +1,34 @@
+# This script will download the latest videos only from a channel.  
+# The first time you run it it will compile a file (archive.txt) that will itemize all videos in that channel.
+# The next time you run it, any videos added since then will download.
+# This also deletes videos older than 3 days (configurable)
+
+$showList = @('@JimmyKimmelLive', '@ColbertLateShow','@TheDailyShow','@LateNightSeth','@LastWeekTonight')
+$numberofdaysoldtokeep = 3
+
+#Delete Shows Older than 48 hours 
+$limit = (Get-Date).AddDays(-$numberofdaysoldtokeep)  
+$path = ".\Talk Shows"
+# Delete files older than the $limit.  Comment the line below if you want to keep the downloads forever
+Get-ChildItem -Path $path -Recurse -Force | Where-Object { !$_.PSIsContainer -and $_.CreationTime -lt $limit } | Remove-Item -Force
+# Delete any empty directories left behind after deleting the old files.
+# Get-ChildItem -Path $path -Recurse -Force | Where-Object { $_.PSIsContainer -and (Get-ChildItem -Path $_.FullName -Recurse -Force | Where-Object { !$_.PSIsContainer }) -eq $null } | Remove-Item -Force -Recurse
+
+#check for yt-dlp updates
+.\yt-dlp.exe -U
+
+If (Test-Path .\archive.txt) {
+    Write-Host "Archive exists - downloading dif"
+        foreach ($i in $showList)
+        {
+            # Download any files not on the list
+            .\yt-dlp.exe -o "/Talk Shows/%(uploader)s/%(playlist_index)s - %(title)s [%(id)s].%(ext)s" --download-archive archive.txt "https://www.youtube.com/$i"
+        }    
+} else {
+        foreach ($i in $showList)
+            {
+                #Build the do-not-download list
+                .\yt-dlp.exe --force-write-archive --simulate --flat-playlist --download-archive archive.txt "https://www.youtube.com/$i"
+            }
+}
+
