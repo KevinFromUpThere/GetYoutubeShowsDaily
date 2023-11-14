9457ff7235406d120da124df7f5aa3eecba93f27
archibk <kevin.archibald@phri.ca>
2023-11-14 13:13:12 -0500

    Changing args

--- GetYoutubeShows.ps1
+++ GetYoutubeShows.ps1
@@ -4,10 +4,10 @@
 # This also deletes videos older than 3 days (configurable)
 
 $showList = @('@JimmyKimmelLive', '@ColbertLateShow','@TheDailyShow','@LateNightSeth','@LastWeekTonight')
-$numberofdaysoldtokeep = 3
+$NumberOfDaysOldToKeep = 3
 
 #Delete Shows Older than 48 hours 
-$limit = (Get-Date).AddDays(-$numberofdaysoldtokeep)  
+$limit = (Get-Date).AddDays(-$NumberOfDaysOldToKeep)  
 $path = ".\Talk Shows"
 # Delete files older than the $limit.  Comment the line below if you want to keep the downloads forever
 Get-ChildItem -Path $path -Recurse -Force | Where-Object { !$_.PSIsContainer -and $_.CreationTime -lt $limit } | Remove-Item -Force
@@ -22,7 +22,22 @@ If (Test-Path .\archive.txt) {
         foreach ($i in $showList)
         {
             # Download any files not on the list
-            .\yt-dlp.exe -o "/Talk Shows/%(uploader)s/%(playlist_index)s - %(title)s [%(id)s].%(ext)s" --download-archive archive.txt "https://www.youtube.com/$i"
+            .\yt-dlp.exe `
+            --no-abort-on-error `
+            -r6M `
+            --mtime `
+            --convert-thumbnails jpg `
+            --write-thumbnail `
+            --merge-output-format mp4 `
+            --write-subs `
+            --sub-format srt `
+            --write-info-json `
+            --write-description `
+            --embed-chapters `
+            --embed-metadata  `
+             archive.txt "https://www.youtube.com/$i" `
+             -o "/Talk Shows/%(uploader)s/%(playlist_index)s - %(title)s [%(id)s].%(ext)s" `
+             -o "thumbnail:/Talk Shows/%(uploader)s/%(playlist_index)s - %(title)s [%(id)s]\poster.jpg" 
         }    
 } else {
         foreach ($i in $showList)
